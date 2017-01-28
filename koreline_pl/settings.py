764762dd8ba836aa22be6d4dc67a4b4e68df0a26
Akus93 <dawid.rdzanek@protonmail.com>
2017-01-28 11:39:47 +0100

    Przygotowanie do inzynierki

--- koreline_pl/settings.py
+++ koreline_pl/settings.py
@@ -191,10 +191,7 @@ OLD_PASSWORD_FIELD_ENABLED = True
 LOGOUT_ON_PASSWORD_CHANGE = False
 
 AUTHENTICATION_BACKENDS = (
- # Needed to login by username in Django admin, regardless of `allauth`
  "django.contrib.auth.backends.ModelBackend",
-
- # `allauth` specific authentication methods, such as login by e-mail
  "allauth.account.auth_backends.AuthenticationBackend",
 )
 
